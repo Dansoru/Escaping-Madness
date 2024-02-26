f3ec5349b0413a345bf83607f2cfb1bffa7f60ca
zReaper59 <javierlopezins@gmail.com>
2024-02-26 15:26:50 +0100

    uno do y tre

---
+++ Proyecto Unity/EscapingMadness.ModuloProyecto/Assets/Scripts/Personaje/Movement.cs
@@ -0,0 +1,108 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Movement : MonoBehaviour
+{
+
+    public CharacterController controller;
+    public GameObject playerCamera;
+
+    public float speed = 12f;
+    public float gravedad = -9.81f;
+
+    public Transform groundCheck;
+    public float groundDistance = 0.4f;
+    public LayerMask groundMask;
+
+    Vector3 velocity;
+    bool isGrounded;
+
+
+    [SerializeField] private float crouchHeight = 0.5f;
+    [SerializeField] private float standHeight = 2f;
+    [SerializeField] private float timeToCrouch = 0.25f;
+    [SerializeField] private Vector3 crouchingCenter = new Vector3(0, 0.5f, 0);
+    [SerializeField] private Vector3 standingCenter = new Vector3(0, 0, 0);
+
+    private bool isCrouching;
+    private bool duringCrouchAnimation;
+    private bool ShouldCrouch => Input.GetKeyDown(crouchKey) && !duringCrouchAnimation && controller.isGrounded;
+
+    [SerializeField] private KeyCode crouchKey = KeyCode.C;
+    [SerializeField] private bool canCrouch = true;
+
+    void Start()
+    {
+        
+    }
+
+    void Update()
+    {
+        isGrounded = Physics.CheckSphere(groundCheck.position, groundDistance, groundMask);
+
+        if (isGrounded && velocity.y < 0)
+        {
+            velocity.y = -2f;
+        }
+
+        float x = Input.GetAxis("Horizontal");
+        float z = Input.GetAxis("Vertical");
+
+        Vector3 move = transform.right * x + transform.forward * z;
+
+        // Reducir la velocidad si el personaje está agachado
+        float currentSpeed = isCrouching ? speed * 0.5f : speed; // Cambia el factor de reducción según lo desees
+        controller.Move(move * currentSpeed * Time.deltaTime);
+
+        velocity.y += gravedad * Time.deltaTime;
+
+        controller.Move(velocity * Time.deltaTime);
+
+
+        if (canCrouch)
+        {
+            HandleCrouch();
+        }
+    }
+
+
+    private void HandleCrouch()
+    {
+        if (ShouldCrouch)
+        {
+            StartCoroutine(CrouchStand());
+        }
+    }
+
+    private IEnumerator CrouchStand()
+    {
+        if (isCrouching && Physics.Raycast(playerCamera.transform.position, Vector3.up, 1f))
+        {
+            yield break;
+        }
+
+        duringCrouchAnimation = true;
+
+        float timeElapsed = 0;
+        float targetHeight = isCrouching ? standHeight : crouchHeight;
+        float currentHeight = controller.height;
+        Vector3 targetCenter = isCrouching ? standingCenter : crouchingCenter;
+        Vector3 currentCenter = controller.center;
+
+        while (timeElapsed < timeToCrouch)
+        {
+            controller.height = Mathf.Lerp(currentHeight, targetHeight, timeElapsed/timeToCrouch);
+            controller.center = Vector3.Lerp(currentCenter, targetCenter, timeElapsed/timeToCrouch);
+            timeElapsed += Time.deltaTime;
+            yield return null;
+        }
+
+        controller.height = targetHeight;
+        controller.center = targetCenter;
+
+        isCrouching = !isCrouching;
+
+        duringCrouchAnimation = false;
+    }
+}
